18235908e36dfdc4f5b09d3c96695d9824761e13
Aidan Do <aidand@semsci.com>
2023-07-11 10:52:28 +0930

    test'

---
+++ core/src/main/java/com/baeldung/core/App.java
@@ -0,0 +1,13 @@
+package com.baeldung.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
